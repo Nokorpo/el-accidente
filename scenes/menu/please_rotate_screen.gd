c00c300c00dc9ea656adafb2b345d9ad6efbd454
antimundo <pablo.rubio.ru@gmail.com>
2024-12-06 21:18:53 +0100

    Update rotate screen scene

--- scenes/menu/please_rotate_screen.gd
+++ scenes/menu/please_rotate_screen.gd
@@ -1,11 +1,18 @@
 extends PanelContainer
 
-func _is_screen_in_portrait() -> bool:
-	var screen_size: Vector2 = DisplayServer.screen_get_size()
-	return (screen_size.x/screen_size.y) < 1
+func _ready() -> void:
+	hide()
 
 func _process(_delta: float) -> void:
 	if _is_screen_in_portrait():
 		show()
 	elif visible:
 		hide()
+
+func _is_screen_in_portrait() -> bool:
+	var screen_size: Vector2
+	if OS.has_feature("editor"):
+		screen_size = DisplayServer.window_get_size()
+	else:
+		screen_size = DisplayServer.screen_get_size()
+	return (screen_size.x/screen_size.y) < 1
