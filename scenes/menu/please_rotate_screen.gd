351e6fadc221098f37f6159d85cb03e9e6db6e58
Edearth <nepormoseno1992@gmail.com>
2024-12-06 15:40:50 +0100

    Linter fixes

--- scenes/menu/please_rotate_screen.gd
+++ scenes/menu/please_rotate_screen.gd
@@ -1,10 +1,10 @@
 extends PanelContainer
 
 func _is_screen_in_portrait() -> bool:
-	var size: Vector2 = DisplayServer.screen_get_size()
-	return (size.x/size.y) < 1
+	var screen_size: Vector2 = DisplayServer.screen_get_size()
+	return (screen_size.x/screen_size.y) < 1
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if _is_screen_in_portrait():
 		show()
 	elif visible:
