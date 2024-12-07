b2814cb5153e2e59b7853d62478d5393af7b4170
Edearth <nepormoseno1992@gmail.com>
2024-12-07 08:57:39 +0100

    Implement custom line UI for marker

---
+++ scenes/player/dash_distance.gd
@@ -0,0 +1,15 @@
+@tool
+extends Marker2D
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		queue_redraw()
+
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		var transform: Transform2D = get_global_transform().inverse()
+		var parent_position: Vector2 = transform * get_parent().global_position
+		draw_line(parent_position, Vector2.ZERO, Color.LIGHT_BLUE, 4)
+		draw_circle(Vector2.ZERO, 10, Color.LIGHT_BLUE)
+		draw_string(ThemeDB.fallback_font, Vector2(-20, -40), "DASH")
+		draw_string(ThemeDB.fallback_font, Vector2(-35, -20), "DISTANCE")
