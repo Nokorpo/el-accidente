6554f9364e554e89710f3ffae641f341ffd55481
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2024-12-07 13:57:35 +0100

    Add blockout placeholder scripts

---
+++ assets/blockout/elevator_platform.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var mov_speed = 100
+var is_moving: bool = false
+
+@onready var destination_position = $DetinationPosition.global_position
+@onready var start_position = $Platform.global_position
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		is_moving = true
+
+func move(delta):
+	if is_moving:
+		$Platform.global_position = $Platform.global_position.move_toward(destination_position, delta*mov_speed)
+	if $Platform.global_position == destination_position:
+		is_moving = false
+
+func _process(delta):
+	move(delta)
