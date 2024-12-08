0e9e580bc6c3c5d6c6d3fa67044f7072ebbdb2f2
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2024-12-08 14:54:10 +0100

    Add camera offset areas

--- scenes/game/camera.gd
+++ scenes/game/camera.gd
@@ -3,5 +3,5 @@ extends Camera2D
 
 @export var player: Player
 
-func _process(_delta: float) -> void:
-	self.global_position = lerp(self.global_position, player.global_position, .9)
+#func _process(_delta: float) -> void:
+	#self.global_position = lerp(self.global_position, player.global_position, .9)
---
+++ scenes/shared/level/camera_offset_area.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var camera: Camera2D
+@export var offset_quantity: float
+
+var initial_offset: float
+var current_offset: float
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	camera.offset.y = lerp(camera.offset.y, current_offset, .01)
+
+func change_offset(body):
+	if body.is_in_group("player"):
+		current_offset = offset_quantity
+
+func reset_offset(body):
+	if body.is_in_group("player"):
+		current_offset = 0
