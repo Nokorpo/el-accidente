13480ed2ff56a3a911693da4779caa8d093a504d
Edearth <nepormoseno1992@gmail.com>
2024-12-09 02:49:59 +0100

    Make player respawn on leaving lvl

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,7 +7,15 @@ const SPEED_FPS_MULTIPLIER: float = 3600
 
 ## Player speed in pixels/second
 @export var speed: float = 10.0
+## At which depth the player will respawn
+@export var respawn_depth_threshold: float = 2000
+
 var is_affected_by_gravity: bool = true
+
+var _checkpoint: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	_checkpoint = global_position
 
 func _physics_process(delta: float) -> void:
 	velocity.x = lerp( velocity.x,  speed * SPEED_FPS_MULTIPLIER * delta, .9)
@@ -15,3 +23,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	move_and_slide()
+	
+	if global_position.y >= respawn_depth_threshold:
+		respawn()
+
+func respawn() -> void:
+	global_position = _checkpoint
