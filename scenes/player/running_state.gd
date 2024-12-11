4007bada519ed7e0ceca372a2ed4d62f00fd1154
Edearth <nepormoseno1992@gmail.com>
2024-12-11 21:45:52 +0100

    Implement flag to prevent dashes on air

--- scenes/player/running_state.gd
+++ scenes/player/running_state.gd
@@ -2,6 +2,8 @@ extends StateMachineState
 class_name RunningState
 ## State that lets the player character continue running, without user input.
 
+@export var can_decelerate_while_falling: bool = true
+
 var _original_speed: float
 
 @warning_ignore("shadowed_variable")
@@ -10,20 +12,23 @@ func _start(sm: StateMachine, node: Node) -> void:
 	_original_speed = node.speed
 
 func _on_enter_state() -> void:
-	if Input.is_action_pressed("action"):
-		state_machine.change_state(DeceleratingState)
-		return
 	node.speed = _original_speed
 	%AnimatedSprite2D.play("running")
 
 func _process(_delta) -> void:
 	if active:
-		if Input.is_action_just_pressed("action"):
-			state_machine.change_state(DeceleratingState)
+		if Input.is_action_pressed("action"):
+			decelerate()
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			state_machine.change_state(DeceleratingState)
+			decelerate()
 
 func _input(event: InputEvent) -> void:
 	if active:
 		if event is InputEventMouseButton and event.is_pressed():
-			state_machine.change_state(DeceleratingState)
+			decelerate()
+
+func decelerate() -> void:
+	if not can_decelerate_while_falling and not node.is_on_floor():
+		# player is not on floor, so they are not able to decelerate
+		return
+	state_machine.change_state(DeceleratingState)
