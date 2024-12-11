744b1dfb445c53c2742766b095f38f7ec8ebe2eb
Edearth <nepormoseno1992@gmail.com>
2024-12-11 20:11:38 +0100

    Touch feedback improvements based on feedback

--- scenes/shared/vfx/touch_feedback_frame.gd
+++ scenes/shared/vfx/touch_feedback_frame.gd
@@ -1,8 +1,13 @@
 extends TextureRect
 
+@onready var original_color: Color = material.get_shader_parameter("color")
+
+func _ready() -> void:
+	material.set_shader_parameter("color", Color.TRANSPARENT)
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			material.set_shader_parameter("color", Color.WHITE)
+			material.set_shader_parameter("color", original_color)
 		else:
 			material.set_shader_parameter("color", Color.TRANSPARENT)
