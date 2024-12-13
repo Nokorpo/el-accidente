fcaf2d0df9aa0a44a1605870071c4377b9cc141f
Edearth <nepormoseno1992@gmail.com>
2024-12-13 23:21:53 +0100

    Implement player animations & Berta driving anims
    Adds a script to stop the player and change it to the car
    animations. It does some code stuff to stop the player at one point
    where the transition is seamless.
    
    This commit also moves the SpriteFrames animations to files in the
    file system. That way we can re-use the animations more easily and
    they can be referenced in code easier.

---
+++ scenes/game/enter_car_animation.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@onready var car_sprite: Sprite2D = $CarSprite
+
+var _player: Player
+var _original_speed: float
+var _already_fired: bool = false
+
+func _ready() -> void:
+	EventBus.reload_level.connect(reset)
+
+func reset() -> void:
+	_player = null
+	_already_fired = false
+
+func _process(_delta: float) -> void:
+	if _player != null and not _already_fired:
+		if _player.global_position.x >= car_sprite.global_position.x:
+			_player.global_position.x = car_sprite.global_position.x
+			_already_fired = true
+			_player.stop_for_animation()
+			$TimeStandingBesidesCar.start()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_player = body
+		_original_speed = _player.speed
+
+func _on_time_standing_besides_car_timeout() -> void:
+	_player.change_sprite_to_car()
+	#_player.speed = _original_speed
+	_player.animation_finished()
+	
