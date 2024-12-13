5158b07cbbcdef1fcf796940b9d74cd98ee76deb
antimundo <pablo.rubio.ru@gmail.com>
2024-12-13 14:03:54 +0100

    Update curtain transition

--- scenes/menu/curtain.gd
+++ scenes/menu/curtain.gd
@@ -1,8 +1,21 @@
 extends Control
 ## Curtain animation used when respawning
 
+@onready var _animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var _timer: Timer = $Timer
+
 func _ready():
-	EventBus.reload_level.connect(_play_curtain_animation)
+	EventBus.play_curtain_animation.connect(_play_curtain_animation)
 
 func _play_curtain_animation():
-	$AnimationPlayer.play("next_level")
+	if _animated_sprite_2d.is_playing():
+		return
+	
+	var animation_duration := _animated_sprite_2d.sprite_frames.get_frame_count("next_level") \
+		/ _animated_sprite_2d.sprite_frames.get_animation_speed("next_level")
+	_animated_sprite_2d.play("next_level")
+	
+	_timer.start(animation_duration / 2.0)
+	await _timer.timeout
+	
+	EventBus.emit_signal("reload_level")
