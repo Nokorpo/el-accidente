8f8a6853d3d3e1766fd5bc45565c5e069a42dcc6
Edearth <nepormoseno1992@gmail.com>
2024-12-14 22:02:38 +0100

    Add flame obstacle sprites

--- scenes/shared/level/flame.gd
+++ scenes/shared/level/flame.gd
@@ -6,6 +6,7 @@ extends Area2D
 
 static var _timer: Timer
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 var _is_active: bool = false
 
 func _ready() -> void:
@@ -16,30 +17,20 @@ func _ready() -> void:
 	_is_active = false
 	_timer.start.call_deferred(time_off)
 
-# TODO placeholder until we have the animations.
-# We will have to subscribe to the animation to know when it finishes
-# before starting the timer. Also we should launch the right loop
-# animation depending on if the fire is on or off
 func toggle_fire() -> void:
 	_is_active = not _is_active
 	if _is_active:
-		# TODO uncomment when we have animations. Keep here, we want monitoring
-		# to be disabled while the animation plays
-		#$AnimationPlayer.play("turn_on")
-		#await $AnimationPlayer.animation_finished
-		#$AnimationPlayer.play("on_loop")
+		sprite.play("turn_on")
+		await sprite.animation_finished
+		sprite.play("on_loop")
 		monitoring = true
 		_timer.start(time_on)
 	else:
 		monitoring = false
-		# TODO uncomment when we have animations. Keep here, we want monitoring
-		# to be disabled while the animation plays
-		#$AnimationPlayer.play("turn_off")
-		#await $AnimationPlayer.animation_finished
-		#$AnimationPlayer.play("off")
+		sprite.play("turn_off")
+		await sprite.animation_finished
+		sprite.play("off")
 		_timer.start(time_off)
-
-	$Flame.visible = _is_active #TODO remove when we have animations
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
