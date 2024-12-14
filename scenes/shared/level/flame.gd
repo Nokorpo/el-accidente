8be4a0be1449783ed678f6e9554a8d2d5d043967
Edearth <nepormoseno1992@gmail.com>
2024-12-14 14:11:23 +0100

    Implement flames obstacle

---
+++ scenes/shared/level/flame.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+@export var time_on: float = 1
+@export var time_off: float = 1
+@export var time_for_transition: float = 0.5
+
+@onready var _timer: Timer = $Timer
+
+var _is_active: bool = false
+
+func _ready() -> void:
+	_timer.timeout.connect(toggle_fire)
+	_is_active = false
+	_timer.start(time_off)
+
+# TODO placeholder until we have the animations.
+# We will have to subscribe to the animation to know when it finishes
+# before starting the timer. Also we should launch the right loop
+# animation depending on if the fire is on or off
+func toggle_fire() -> void:
+	_is_active = not _is_active
+	if _is_active:
+		# TODO uncomment when we have animations. Keep here, we want monitoring
+		# to be disabled while the animation plays
+		#$AnimationPlayer.animation_finished.connect(toggle_fire)
+		monitoring = true
+		_timer.start(time_on)
+	else:
+		monitoring = false
+		# TODO uncomment when we have animations. Keep here, we want monitoring
+		# to be disabled while the animation plays
+		#$AnimationPlayer.animation_finished.connect(toggle_fire)
+		_timer.start(time_off)
+
+	$Flame.visible = _is_active #TODO remove when we have animations
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.die()
