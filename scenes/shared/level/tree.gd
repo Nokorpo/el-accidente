1b75de14043423f636b68ac5cfb532365ff3beed
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2024-12-15 13:57:40 +0100

    Add tree scene

---
+++ scenes/shared/level/tree.gd
@@ -0,0 +1,18 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		play("fire")
+		body.process_mode =Node.PROCESS_MODE_DISABLED
+		body.hide()
