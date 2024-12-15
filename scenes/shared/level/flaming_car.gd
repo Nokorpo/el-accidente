b29938bb384ca8b748bf6f2ece69637ba9957d2e
Edearth <nepormoseno1992@gmail.com>
2024-12-15 02:14:36 +0100

    Add flaming car

---
+++ scenes/shared/level/flaming_car.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	EventBus.reload_level.connect(reset)
+	reset()
+
+func reset() -> void:
+	#$AnimationPlayer.play("RESET")
+	$CollisionArea.monitoring = true
+
+func _on_collision_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.die()
+
+func _on_triggered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		$AnimationPlayer.play("run")
