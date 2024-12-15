6ac45fa8cbf3614853f2dff0316fc37ef7df4c9c
antimundo <pablo.rubio.ru@gmail.com>
2024-12-16 00:26:27 +0100

    Remove unused camera script

--- scenes/game/camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-## Follows the player character
-
-@export var player: Player
-
-#func _process(_delta: float) -> void:
-	#self.global_position = lerp(self.global_position, player.global_position, .9)
