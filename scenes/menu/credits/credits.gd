4cba5a9dab30af1f3f53a161292e961f6c502e42
antimundo <pablo.rubio.ru@gmail.com>
2024-12-16 13:24:20 +0100

    Add nokorpo credits

--- scenes/menu/credits/credits.gd
+++ scenes/menu/credits/credits.gd
@@ -5,3 +5,9 @@ func _ready() -> void:
 
 func _on_close_pressed() -> void:
 	set_visible(false)
+
+func _on_more_pressed() -> void:
+	$More.set_visible(true)
+
+func _on_close_more_pressed() -> void:
+	$More.set_visible(false)
